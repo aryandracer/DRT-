caaf6ca692435ea9a0984a1d36399204342a4da3
aryandracer <8tx2q4tm42@privaterelay.appleid.com>
2026-01-23 02:21:50 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>DRT Consultancy & Legal Advisory</h1>
+      <p>Home page is working.</p>
+    </div>
+  );
+}
